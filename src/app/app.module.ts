d4fcde7077041424b4956f52a1737b96518a3b91
Brian Kang <bhkangw@gmail.com>
2018-01-17 10:34:30 -0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { EmailsComponent } from './emails/emails.component';
@@ -12,7 +13,9 @@ import { EmailsComponent } from './emails/emails.component';
     EmailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
